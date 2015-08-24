f9e43ad957e2c2929481121da4a0cf1979805914
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-24 14:42:31 -0600

    JS map

--- 02-map/script.js
+++ 02-map/script.js
@@ -1,7 +1,17 @@
 // TODO: Create an array called `simplifiedPhotographs` that takes `photographs` and transforms each one into an object with only two properties: `caption` and `url`.
 
-var simplifiedPhotographs = photographs;
+var simplifiedPhotographs = photographs.map(function(photo) {
+  return {
+    caption: photo.caption.text,
+    url: photo.images.low_resolution.url
+  };
+});
 
 // TODO: Create an array called `photographElements` that takes `simplifiedPhotographs` and transforms each one into a DOM element.
 
-var photographElements = simplifiedPhotographs;
+var photographElements = simplifiedPhotographs.map(function(photo){
+  var p = document.createElement("img");
+  p.alt = photo.caption;
+  p.src = photo.url;
+  return p;
+});
